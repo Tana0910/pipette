c1281b90e795908570aab07447da5f99792673b6
Tana0910 <32482176+Tana0910@users.noreply.github.com>
2024-07-30 21:41:56 +0900

    :construction: the application factory
     Changes to be committed:
    	modified:   pipette/__init__.py

--- pipette/__init__.py
+++ pipette/__init__.py
@@ -1,7 +1,31 @@
+import os
+
 from flask import Flask
 
-app = Flask(__name__)
+def create_app(test_coding=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'pipette.sqlite'),
+    )
 
-@app.route("/")
-def hello():
-    return 'Hello World!'
+    if test_coding is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_coding)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simaple page thet says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+    
+    return app
